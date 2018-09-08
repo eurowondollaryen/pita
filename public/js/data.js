52912cb990d4eb4ec0f7aa573a8f206873165da7
eurowondollaryen <sehoakasayho@naver,com>
2018-09-09 00:23:02 +0900

    prototype
    ok

---
+++ public/js/data.js
@@ -0,0 +1,160 @@
+//자음, 모음의 숫자값들이 정리되어있는 파일
+var ja = [
+	{
+		kor: "ㄱ",
+		num: 1
+	},
+	{
+		kor: "ㄴ",
+		num: 2
+	},
+	{
+		kor: "ㄷ",
+		num: 3
+	},
+	{
+		kor: "ㄹ",
+		num: 4
+	},
+	{
+		kor: "ㅁ",
+		num: 5
+	},
+	{
+		kor: "ㅂ",
+		num: 6
+	},
+	{
+		kor: "ㅅ",
+		num: 7
+	},
+	{
+		kor: "ㅇ",
+		num: 8
+	},
+	{
+		kor: "ㅈ",
+		num: 9
+	},
+	{
+		kor: "ㅊ",
+		num: 1
+	},
+	{
+		kor: "ㅋ",
+		num: 2
+	},
+	{
+		kor: "ㅌ",
+		num: 3
+	},
+	{
+		kor: "ㅍ",
+		num: 4
+	},
+	{
+		kor: "ㅎ",
+		num: 5
+	}
+];
+
+var mo = [
+	{
+		kor: "ㅏ",
+		num: 1
+	},
+	{
+		kor: "ㅑ",
+		num: 2
+	},
+	{
+		kor: "ㅓ",
+		num: 3
+	},
+	{
+		kor: "ㅕ",
+		num: 4
+	},
+	{
+		kor: "ㅗ",
+		num: 5
+	},
+	{
+		kor: "ㅛ",
+		num: 6
+	},
+	{
+		kor: "ㅜ",
+		num: 7
+	},
+	{
+		kor: "ㅠ",
+		num: 8
+	},
+	{
+		kor: "ㅡ",
+		num: 9
+	},
+	{
+		kor: "ㅣ",
+		num: 1
+	}
+];
+//여기까지 자음 모음 매칭 숫자값
+
+var terms = [
+	{
+		journey: 1,
+		term1: [0, 26],
+		term2: [26, 27],
+		term3: [53, 54]
+	},
+	{
+		journey: 2,
+		term1: [0, 25],
+		term2: [25, 26],
+		term3: [52, 53]
+	},
+	{
+		journey: 3,
+		term1: [0, 33],
+		term2: [33, 34],
+		term3: [60, 61]
+	},
+	{
+		journey: 4,
+		term1: [0,32],
+		term2: [32,33],
+		term3: [59,60]
+	},
+	{
+		journey: 5,
+		term1: [0,31],
+		term2: [31,32],
+		term3: [58,59]
+	},
+	{
+		journey: 6,
+		term1: [0,30],
+		term2: [30,31],
+		term3: [57,58]
+	},
+	{
+		journey: 7,
+		term1: [0,29],
+		term2: [29,30],
+		term3: [56,57]
+	},
+	{
+		journey: 8,
+		term1: [0,28],
+		term2: [28,29],
+		term3: [55,56]
+	},
+	{
+		journey: 9,
+		term1: [0,27],
+		term2: [27,28],
+		term3: [54,55]
+	}
+];
