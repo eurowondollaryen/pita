3a9314ed5a18508094f06b698ff652f9940c7715
eurowondollaryen <sehoakasayho@naver,com>
2018-09-09 00:39:03 +0900

    ..
    dd

--- app.js
+++ app.js
@@ -2,23 +2,23 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var path = require('path');
+const port = process.env.PORT || 3000;
 
-const port = process.env.port || 3000;
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(port, () => {
-	console.log('Server is running on ' + port);
+app.use(express.static(path.join(__dirname,'public')));
+//port set
+app.listen(port,function(){
+           console.log('Server is running on  port ' + port);
 });
 
-app.get("/", (req, res) => {
-	fs.readFile('index.html', (error, data) => {
-		if(error) {
-			console.log(error);
-		}
-		else {
-			res.writeHead(200, {'Content-Type' : 'text/html'});
-			res.end(data);
-		}
-	});
+//routing set
+app.get('/',function(req,res){
+    fs.readFile('index.html',function(error,data){
+        if(error) {
+            console.log(error);
+        }
+        else {
+            res.writeHead(200,{'Content-Type':'text/html'});//set headtype
+            res.end(data);
+        }
+    });
 });
